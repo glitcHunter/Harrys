40a0942434d383c6fdb1876dc2af9bfadafb73e4
glitcHunter <Your Email Address>
2015-11-11 19:14:06 +0100

    modyfikacja jestu render_to_string  views render(request,home.html)

--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    '''odpowiedź na żądanie'''
-    return HttpResponse('<html><title>lista</title></html>')
+    return render(request,'home.html')
