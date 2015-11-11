188128385eee1d4c5ebf6c8eff9331b39f7a9d9a
glitcHunter <Your Email Address>
2015-11-11 19:37:18 +0100

    test funkcjonalny i template

--- functional_tests.py
+++ functional_tests.py
@@ -18,20 +18,19 @@ class NewVisitorTest(unittest.TestCase):
         header_text =self.browser.find_element_by_tag_name("h1").text
         self.assertIn('lista',header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'),'wpisz rzeczy do zrobienia')
+        inputbox = self.browser.find_element_by_id('id_new_item') #znajdź inpuy by ud
+        self.assertEqual(inputbox.get_attribute('placeholder'),'wpisz rzeczy do zrobienia') #sprawdź czy w formie znajdujie się
+        #atrybut placeholder z wartością wpisz...
 
-        inputbox.send_keys('buy')
+        inputbox.send_keys('buy')# wyślij buy
 
-        inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER) # wyślij enter
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text =='1: buy' for row in rows)
-        )
+        table = self.browser.find_element_by_id('id_list_table') #znajdź table o id list_table
+        rows = table.find_elements_by_tag_name('tr')#znajdź rows wedlug taga tr
+        self.assertTrue(any(row.text =='1: buy' for row in rows),'nie ma w tabeli')#przeszukaj any wiersz i znjadź 1:buy albo zwróć błędu
 
-        self.fail('zalończenie testu')
+        #self.fail('zalończenie testu')
 
 if __name__ == '__main__':
     unittest.main()
